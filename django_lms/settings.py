2faab836d1d60d1bd7828a809525741fbc7869f6
Peter Kyale <kyalepeter2000@gmail.com>
2023-12-21 17:05:00 +0300

    merged changes from mickmacha

--- django_lms/settings.py
+++ django_lms/settings.py
@@ -169,7 +169,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django_lms3',
         'USER': 'root',
-        'PASSWORD': 'lms123',
+        'PASSWORD': 'password',
         'HOST': 'localhost',
         'PORT': '3306',
     }
